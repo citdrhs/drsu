10fff038732035661a5add574087864811bb02a0
ApplePie-thon <110473258+ApplePie-thon@users.noreply.github.com>
2025-04-16 21:56:22 -0400

    Added logout functionality

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from datetime import datetime
+from werkzeug.utils import secure_filename
+from flask import flash
 
 # Load variables from .env
 load_dotenv()
@@ -44,7 +46,100 @@ def create_app():
         return date_obj.strftime(f"%A, %B {day}{suffix}, %Y")
 
     
+    #Admin Code update for login
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        form = LoginForm()
+        print("before validate event")
 
+        if request.method == 'POST':
+            if form.validate_on_submit:
+                print("inside validate_on_submit")
+                try:
+                    user = User.query.filter_by(email=form.email.data).first()
+                    if user:
+                        print("User exists")
+                        if bcrypt.check_password_hash(user.password, form.password.data):
+                            is_admin = user.is_admin
+
+                            
+                            if form.adminCode.data:
+                                try:
+                                    admin_code = int(form.adminCode.data)
+                                    if admin_code == int(os.getenv('ADMIN_CODE')):
+                                        print("You are an admin!")
+                                        user.is_admin = True
+                                        db.session.commit()
+                                        is_admin = True
+                                except ValueError:
+                                    flash("Invalid admin code format.", "danger")
+
+                            
+                            session['email'] = user.email
+                            session['is_admin'] = is_admin
+                            print("successfully validated user")
+                            return redirect(url_for('homePage'))
+                        else:
+                            flash("Incorrect password. Please try again.", "danger")
+                            print("Incorrect password")
+                    else:
+                        flash("Email does not exist. Please sign up or try again.", "danger")
+                        print("Email does not exist")
+                except Exception as e:
+                    print(e)
+                    flash("An error occurred while trying to log in. Please try again.", "danger")
+
+        
+        return render_template("login.html", form=form)
+    
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for('index'))
+    
+    @app.route("/register", methods=['GET', 'POST'])
+    def register():
+        form = RegisterForm()
+
+        if request.method == 'POST':
+            if form.validate_on_submit:
+                existing_user = User.query.filter_by(email=form.email.data).first()
+                if existing_user:
+                    flash("Email is already in use. Please choose a different one.", "danger")
+                    return redirect(url_for('register'))
+                
+                if form.password.data != form.confirmPassword.data:
+                    flash("Passwords do not match!", "danger")
+                    return redirect(url_for('register'))
+
+                
+                if len(form.password.data) < 8:
+                    flash("Password must be at least 8 characters long.", "danger")
+                    return redirect(url_for('register'))
+
+                
+                hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+                user = User(
+                    email=form.email.data,
+                    first_name=form.first_name.data,
+                    last_name=form.last_name.data,
+                    password=hashed_password,
+                    grade=form.grade.data,
+                    is_admin=form.is_admin.data
+                )
+
+                try:
+                    db.session.add(user)
+                    db.session.commit()
+                    flash("You have been successfully registered!", "success")
+                    return redirect(url_for('index'))
+
+                except Exception as e:
+                    db.session.rollback()
+                    #flash(f"An error occurred: {e}", "danger")
+                    print(e)
+            
+        return render_template("register.html", form=form)
 
     #Display home page
     @app.route("/homePage")
@@ -56,7 +151,7 @@ def create_app():
         cur = conn.cursor()
         
         #Grabs list of events to display in event menu on home page
-        cur.execute('SELECT * FROM events;')
+        cur.execute('SELECT t1.*,t2.first_name,t2.last_name FROM events AS t1 JOIN public.user AS t2 ON t2.email=t1.contact;')
         events = cur.fetchall()
         #Print statement for TESTING
         print(events)
@@ -76,7 +171,9 @@ def create_app():
                 event[4],  # End Time
                 event[5],  # Location
                 event[6],  # Email
-                event[7]   # Background image filename
+                event[7],   # Background image filename
+                event[8],
+                event[9]
             )
             formatted_dates.append(updated_event)
         #Print statement for TESTING
@@ -88,44 +185,45 @@ def create_app():
         #    adminStatus = True
         
         #Passes in list of events (with formatted dates) and admin status to home page
-        return render_template("index.html", events = formatted_dates, admin = is_admin)
+        return render_template("index.html", events = formatted_dates, admin = is_admin, email = session.get('email'))
     
 
 
     
     @app.route("/add_event", methods=['POST'])
     def add_event():
-        print("entered 1")
         event_name = request.form['event_name']
         event_date = request.form['event_date']
         start_time = request.form['start_time']
         end_time = request.form['end_time']
         location = request.form['location']
         contact_email = request.form['contact_email']
-        background_image = request.form['background_image']  # Just the file name, not upload
+
+        background_file = request.files['background_image']
+        filename = secure_filename(background_file.filename)
+
+        basedir = os.path.dirname(__file__)
+        image_folder = os.path.join(basedir, 'static', 'images')
+        background_file.save(os.path.join(image_folder, filename))
 
         try:
-            print("entered 2")
-
-            #Connect to database
             conn = get_db_connection()
             cur = conn.cursor()
 
             cur.execute("""
                 INSERT INTO events (name, date, start_time, end_time, location, contact, file_name)
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
-            """, (event_name, event_date, start_time, end_time, location, contact_email, background_image))
+            """, (event_name, event_date, start_time, end_time, location, contact_email, filename))
 
-            #Commit changes to database and close database connection
             conn.commit()
             cur.close()
             conn.close()
 
         except Exception as e:
-            print("entered 3")
             print("Database error:", e)
 
         return redirect(url_for('homePage'))
+
 
 
 
@@ -157,7 +255,7 @@ def create_app():
         cur.execute('SELECT * from events where id = %s', (event_id,))
         event_data = cur.fetchall()
         print(event_data)
-
+        
         #Formats date name to text, ex. 2012-04-13 --> April 13th, 2012
         formatted_date = format_date_with_suffix(event_data[0][2])
         updated_event = (
@@ -172,11 +270,6 @@ def create_app():
         )
         #Print statement for TESTING
         print(updated_event)
-
-        '''
-        filename = event_data[0][0].replace(' ', '-').lower() + '-' + str(event_data[0][1]) + '.html'
-        print(filename)
-        '''
 
         cur.execute('SELECT * FROM event_tables WHERE event_id = %s order by vieworder', (event_id,))
         tables = cur.fetchall()
@@ -234,83 +327,6 @@ def create_app():
             email = session_email
         )
 
-    
-
-
-    @app.route('/delete_signup/<int:id>/')
-    def delete_signup(id):
-
-        print("delete signup")
-
-        #Connect to database
-        conn = get_db_connection()
-        cur = conn.cursor()
-
-        cur.execute('SELECT t1.event_id FROM event_tables as t1 Join event_signups as t2 On t1.id=t2.table_id WHERE t2.id = %s', (id,))
-        #Data is in within a tuple inside a list: [(id)] so cur.fetchall()[0][0] is needed to grab the actual number
-        eventID = cur.fetchall()[0][0]
-        cur.execute('DELETE FROM event_signups WHERE id = %s', (id,))
-        
-        #Commit changes to database and close database connection
-        conn.commit()
-        cur.close()
-        conn.close()
-
-        return redirect(url_for('viewEvent', event_id = eventID))
-
-
-
-    @app.route('/delete_table/<int:id>/')
-    def delete_table(id):
-
-        #Connect to database
-        conn = get_db_connection()
-        cur = conn.cursor()
-
-        cur.execute('SELECT event_id FROM event_tables WHERE id = %s', (id,))
-        #Data is in within a tuple inside a list: [(id)] so cur.fetchall()[0][0] is needed to grab the actual number
-        eventID = cur.fetchall()[0][0]
-        cur.execute('DELETE FROM event_tables WHERE id = %s', (id,))
-        
-        #Commit changes to database and close database connection
-        conn.commit()
-        cur.close()
-        conn.close()
-
-        return redirect(url_for('viewEvent', event_id = eventID))
-
-
-
-
-    @app.route("/add_table/<int:event_id>", methods=['POST'])
-    def add_table(event_id):
-        print("entered add table")
-        category = request.form['category']
-        max_entries = request.form['max_entries']
-        vieworder = request.form['vieworder']
-
-        try:
-            print("entered try")
-            conn = get_db_connection()
-            cur = conn.cursor()
-            cur.execute("""
-                INSERT INTO event_tables (event_id, table_name, maxentries,vieworder)
-                VALUES (%s, %s, %s, %s)
-            """, (event_id, category, max_entries,vieworder,))
-
-            #Commit changes to database and close database connection
-            conn.commit()
-            cur.close()
-            conn.close()
-
-        except Exception as e:
-            print("Database error:", e)
-
-        return redirect(url_for('viewEvent', event_id = event_id))
-
-
-
-
     @app.route('/signup/<int:table_id>', methods=['POST'])
     def signup(table_id):
         #Email taken from the current user's email 
@@ -337,7 +353,7 @@ def create_app():
         person = nameData[0][0] + ' ' + nameData[0][1]
 
         cur.execute('INSERT INTO event_signups (table_id, responsible_name, item_name, responsible_email, comment) VALUES (%s, %s, %s, %s, %s)', (table_id, person, dish, email, comment))
- 
+
         #Commit changes to database and close database connection
         conn.commit()
         cur.close()
@@ -346,77 +362,73 @@ def create_app():
         return redirect(url_for('viewEvent', event_id = eventID))
 
 
+    @app.route('/delete_signup/<int:id>/')
+    def delete_signup(id):
 
+        print("delete signup")
 
-    #Admin Code update for login
-    @app.route("/", methods=['GET','POST'])
-    def index():
-        form = LoginForm()
-        print("before validate event")
-        if form.validate_on_submit:
-            print("inside validate_on_submit")
-            try:
-                user = User.query.filter_by(email=form.email.data).first()
-                print("successfully logged in")
-                if user and bcrypt.check_password_hash(user.password, form.password.data):
-                    is_admin=user.is_admin
-                    #if 'adminCode' in session and session['adminCode'] == app.config['ADMIN_CODE']:
-                    if form.adminCode.data and form.adminCode.data == int(os.getenv('ADMIN_CODE')):
-                        print("You are an admin!")
-                        user.is_admin=True
-                        db.session.commit()
-                        is_admin=True
-                    else:
-                        is_admin=user.is_admin
-                    session['email'] = user.email
-                    session['is_admin'] = is_admin
-                    db.session.commit()
-                    print("successfully validated user")
-                    return redirect(url_for('homePage'))
-                    #return render_template("homePage.html", is_admin=is_admin)
-                else:
-                    print("Error validating user. Please check email and password")
-            except Exception as e:
-                print(e)
-            
-        else:
-            return render_template("login.html", form = form)
-        return render_template("login.html", form = form)
-    
+        #Connect to database
+        conn = get_db_connection()
+        cur = conn.cursor()
 
+        cur.execute('SELECT t1.event_id FROM event_tables as t1 Join event_signups as t2 On t1.id=t2.table_id WHERE t2.id = %s', (id,))
+        #Data is in within a tuple inside a list: [(id)] so cur.fetchall()[0][0] is needed to grab the actual number
+        eventID = cur.fetchall()[0][0]
+        cur.execute('DELETE FROM event_signups WHERE id = %s', (id,))
+        
+        #Commit changes to database and close database connection
+        conn.commit()
+        cur.close()
+        conn.close()
 
+        return redirect(url_for('viewEvent', event_id = eventID))
 
-    @app.route("/register", methods=['GET','POST'])
-    def register():
-        print("entered register")
-        form = RegisterForm()
-        if form.email.data is not None and form.password.data is not None:
-            print("entered first if")
-            if form.validate_on_submit:
-                print("entered second if")
-                hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-                user = User(
-                    email=form.email.data,
-                    first_name=form.first_name.data,
-                    last_name=form.last_name.data,
-                    password=hashed_password,
-                    grade=form.grade.data,
-                    is_admin=form.is_admin.data
-                )
-                try:
-                    print("entered try")
-                    db.session.add(user)
-                    db.session.commit()
-                    return redirect(url_for('index'))
-                    #return render_template("login.html", form = form)
-                except Exception as e:
-                    db.session.rollback()
-                    print(e)
+    @app.route('/delete_table/<int:id>/')
+    def delete_table(id):
 
-        return render_template("register.html", form = form)
+        #Connect to database
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute('SELECT event_id FROM event_tables WHERE id = %s', (id,))
+        #Data is in within a tuple inside a list: [(id)] so cur.fetchall()[0][0] is needed to grab the actual number
+        eventID = cur.fetchall()[0][0]
+        cur.execute('DELETE FROM event_tables WHERE id = %s', (id,))
+        
+        #Commit changes to database and close database connection
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('viewEvent', event_id = eventID))
+
+    @app.route("/add_table/<int:event_id>", methods=['POST'])
+    def add_table(event_id):
+        print("entered add table")
+        category = request.form['category']
+        max_entries = request.form['max_entries']
+        vieworder = request.form['vieworder']
+
+        try:
+            print("entered try")
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute("""
+                INSERT INTO event_tables (event_id, table_name, maxentries,vieworder)
+                VALUES (%s, %s, %s, %s)
+            """, (event_id, category, max_entries,vieworder,))
+
+            #Commit changes to database and close database connection
+            conn.commit()
+            cur.close()
+            conn.close()
+
+        except Exception as e:
+            print("Database error:", e)
+
+        return redirect(url_for('viewEvent', event_id = event_id))
     
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
